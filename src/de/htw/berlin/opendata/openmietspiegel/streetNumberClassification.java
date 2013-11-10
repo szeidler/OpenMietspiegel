97c329694597c6cf0758217eed697745ff6c1d1e
Stephan Zeidler <stephanzeidler@web.de>
2013-11-10 17:38:55 +0100

    add first idea of basic classes

---
+++ src/de/htw/berlin/opendata/openmietspiegel/streetNumberClassification.java
@@ -0,0 +1,9 @@
+package de.htw.berlin.opendata.openmietspiegel;
+
+public enum StreetNumberClassification {
+	// K = Komplette Straße
+	// F = Fortlaufende Hausnummern
+	// G = Gerade Hausnummern
+	// U = Ungerade Hausnummern
+	K, F, G, U;
+}
