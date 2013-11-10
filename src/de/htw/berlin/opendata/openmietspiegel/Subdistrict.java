63294619b32945d9790780fbd7bdb5fcf5452b76
Stephan Zeidler <stephanzeidler@web.de>
2013-11-10 19:19:05 +0100

    add parent district relationship to SubDistrict

--- src/de/htw/berlin/opendata/openmietspiegel/Subdistrict.java
+++ src/de/htw/berlin/opendata/openmietspiegel/Subdistrict.java
@@ -1,4 +1,14 @@
 package de.htw.berlin.opendata.openmietspiegel;
 
 public class SubDistrict extends District {
+
+	private District parent;
+
+	public District getParent() {
+		return parent;
+	}
+
+	public void setParent(District parent) {
+		this.parent = parent;
+	}
 }
